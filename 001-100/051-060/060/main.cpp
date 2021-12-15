c5d070d51b8da5d1c9133549507e7ad0f584cef9
danielccxxiv <danielccxxiv@gmail.com>
2021-12-15 08:03:34 -0500

    0

--- 001-100/051-060/060/main.cpp
+++ 001-100/051-060/060/main.cpp
@@ -2,6 +2,73 @@
 #include "main.hpp"
 
 int main() {
-    std::cout << miller_rabin_det<int32_t>(1) << std::endl;
+    prime_node root(-1);
+    size_t pos_iter = 0;
+    int32_t max_depth = 0;
+    while(max_depth < 5) {
+        prime<int32_t>::list.push_back(prime<int32_t>::iter.next_prime());
+        break;
+    }
 	return 0;
 }
+
+void prime_node_loop(prime_node& base_node, int32_t prime_pos_next, std::vector<prime_node*>& storage) {
+    boost::heap::priority_queue<prime_node_pq_wrap, boost::heap::compare<prime_node_c>> queue;
+    boost::heap::priority_queue<prime_node_pq_wrap, boost::heap::compare<prime_node_c>>::iterator queue_iter;
+    queue.push(prime_node_pq_wrap(base_node));
+    prime_node* node_create_ptr;
+    prime_node* node_modify_ptr;
+    std::stack<prime_node*> next_nodes;
+    for(int32_t i = 0; i < prime_pos_next; i++) {
+        while(queue.top().val < i) {
+            node_modify_ptr = queue.top().node;
+            queue.pop();
+            node_create_ptr = new prime_node(prime_pos_next, node_modify_ptr);
+            (node_modify_ptr->next).push_back(node_create_ptr);
+            if((node_create_ptr->level == 5)) {
+                storage.push_back(node_create_ptr);
+            }
+        }
+        if(!(connected(i, prime_pos_next))) {
+            continue;
+        }
+        for(queue_iter = queue.begin(); queue_iter != queue.end(); queue_iter++) {
+            node_modify_ptr = (*iter).node;
+            while(((node_modify_ptr->next)[node_modify_ptr->vec_iter]).prime_pos < i) {
+                (node_modify_ptr->vec_iter)++;
+            }
+            if(((node_modify_ptr->next)[node_modify_ptr->vec_iter]).prime_pos == i) {
+                next_nodes.push((node_modify_ptr->next)[node_modify_ptr->vec_iter]);
+            }
+        }
+        while(next_nodes.size() > 0) {
+            queue.push(prime_node_pq_wrap(next_nodes.top()));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- 001-100/051-060/060/main.hpp
+++ 001-100/051-060/060/main.hpp
@@ -2,9 +2,84 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
 
-#include "../../../Headers/integer_numeric_types.hpp"
+#include "../../../Headers/std_integer_numeric_types.hpp"
 #include <iostream>
 
-#include "../../../Misc/Miller_Rabin/miller_rabin_deterministic.hpp"
+#include <vector>
+
+#include <boost/heap/priority_queue.hpp>
+
+// #include "../../../Misc/Miller_Rabin/miller_rabin_deterministic.hpp"
+#include "../../../Misc/Sequences/prime_sequence.hpp"
+
+typedef struct prime_node {
+    int32_t prime_pos;
+    int32_t level;
+    std::vector<prime_node*> next;
+    int32_t vec_iter;
+    prime_node* parent;
+
+    prime_node(int32_t num, prime_node* parent = NULL) {
+        this->num = num;
+        this->parent = parent;
+        if(parent) {
+            this->level = parent->level + 1;
+        } else {
+            this->level = 0;
+        }
+    }
+
+    ~prime_node() {
+        for(size_t i = 0; i < next.size(); i++) {
+            delete next[i];
+        }
+        next.clear();
+    }
+} prime_node;
+
+typedef struct prime_node_pq_wrap {
+    prime_node* node;
+    int32_t val;
+
+    prime_node_pq_wrap(prime_node* node) {
+        this->node = node;
+        node->vec_iter = 0;
+        if((node->next).size() > 0) {
+            this->val = node->next[(node->next).size() - 1].prime_pos;
+        } else {
+            this->val = 0;
+        }
+    }
+} prime_node_pq_wrap;
+
+typedef struct prime_node_c {
+    bool operator()(const prime_node_pq_wrap& pn0, const prime_node_pq_wrap& pn1) const {
+        return (pn0.val > pn1.val);
+    }
+} prime_node_c;
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
