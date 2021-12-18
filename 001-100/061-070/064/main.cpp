6f785467f0d1cf7ead49fe762d81a5fd3129e479
danielccxxiv <danielccxxiv@gmail.com>
2021-12-18 10:22:28 -0500

    0

---
+++ 001-100/061-070/064/main.cpp
@@ -0,0 +1,39 @@
+
+#include "main.hpp"
+
+int main() {
+    int32_t count = 0;
+    for(int32_t i = 1; i < 10001; i++) {
+        if(cont_frac_period(i) % 2 == 1) {
+            count++;
+        }
+    }
+    std::cout << count << std::endl;
+	return 0;
+}
+
+int32_t cont_frac_period(int32_t n) {
+    alg_num base(n, -floor_sqrt(n), 1);
+    if((base.k * base.k) == n) {
+        return 0;
+    }
+    int32_t count = 1;
+    alg_num next = inverse(base);
+    next.k -= (next.d * alg_num_floor(next));
+    while(next != base) {
+        count++;
+        next = inverse(next);
+        next.k -= (next.d * alg_num_floor(next));
+    }
+    return count;
+}
+
+int32_t alg_num_floor(alg_num num) {
+    return (floor_sqrt(num.n) + num.k) / num.d;
+}
+
+alg_num inverse(alg_num num) {
+    num.d = (num.n - num.k * num.k) / num.d;
+    num.k *= -1;
+    return num;
+}
